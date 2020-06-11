e8a4093aa5db1dcb8e1acda69febd0f593cf1895
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-06-12 01:40:38 +0800

    Matrix operations.

---
+++ R/operations.R
@@ -0,0 +1,30 @@
+#' Trace of a square matrix
+#'
+#' Sum of the diagonal of a square matrix.
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#' @inheritParams is.symmetric
+#' @return
+#'   Returns the sum of the diagonal of a square matrix.
+#' @family matrix functions
+#' @keywords matrix
+#' @examples
+#' Sigma <- matrix(
+#'   data = c(
+#'     225, 112.50, 56.25,
+#'     112.5, 225, 112.5,
+#'     56.25, 112.50, 225
+#'   ),
+#'   ncol = 3
+#' )
+#' tr(X = Sigma)
+#' @references
+#'   [Wikipedia: Trace (Linear Algebra)](https://en.wikipedia.org/wiki/Trace_(linear_algebra))
+#' @export
+tr <- function(X) {
+  square <- is.square(
+    X = X,
+    stop = TRUE
+  )
+  sum(diag(X))
+}
