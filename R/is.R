1a572725f916e0e424ef41d9b7c7e5484c2cb7e6
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-07-31 13:49:42 +0800

    Added is.diag

--- R/is.R
+++ R/is.R
@@ -1,30 +1,38 @@
-#' Square Matrix
-#'
-#' @description Checks if a matrix is square.
-#'
 #' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Square Matrix
+#'
+#' @description Checks if a matrix is square \eqn{\left( A_{m \times m} \right)} .
+#'
 #' @param X Numeric matrix.
 #' @param stop Logical.
 #'   If `TRUE`,
 #'   stops and returns an error if `X` is not a square matrix.
+#'   If `FALSE`,
+#'   returns the result of the test.
 #' @return
-#'   Returns `TRUE`,
-#'   if the number of row and columns in `X` are equal.
-#'   Returns `FALSE`,
+#'   If `stop = FALSE`,
+#'   returns `TRUE`,
+#'   if the number of row and columns in `X` are equal, or
+#'   returns `FALSE`,
 #'   if the number of row and columns in `X` are not equal.
 #' @examples
 #' A <- matrix(
 #'   data = 1:9,
 #'   nrow = 3
 #' )
+#' dim(A)
 #' B <- matrix(
 #'   data = 1:10,
 #'   ncol = 2
 #' )
+#' dim(B)
 #' # Returns TRUE
 #' is.square(X = B)
 #' # Returns FALSE
 #' is.square(X = A)
+#' @references
+#' [Wikipedia: Square matrix](https://en.wikipedia.org/wiki/Square_matrix)
 #' @export
 is.square <- function(X,
                       stop = FALSE) {
@@ -39,7 +47,78 @@ is.square <- function(X,
   test
 }
 
-#' Symmetric Matrix
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Diagonal Matrix
+#'
+#' @description Checks if off-diagonal elements of a square matrix are all zeroes.
+#'
+#' @details The off-diagonal elements are added and compared to a tolerance value.
+#'   If the sum is less than or equal to the tolerance value,
+#'   all the elements are assumed to be zeroes.
+#'
+#' @inheritParams is.square
+#' @param X Square numeric matrix.
+#' @param tol Numeric.
+#'   Tolerance.
+#' @param stop Logical.
+#'   If `TRUE`,
+#'   stops and returns an error if `X` is not a diagonal matrix.
+#'   If `FALSE`,
+#'   returns the result of the test.
+#' @return
+#'   If `stop = FALSE`,
+#'   returns `TRUE`,
+#'   if the sum of the off-diagonal elements of `X` is equal to zero, or
+#'   returns `FALSE`,
+#'   if the sum of the off-diagonal elements of `X` is NOT equal to zero.
+#' @examples
+#' A <- matrix(
+#'   data = 1:9,
+#'   nrow = 3
+#' )
+#' B <- matrix(
+#'   data = 1:10,
+#'   ncol = 2
+#' )
+#' C <- diag(3)
+#' # Returns TRUE
+#' is.diag(X = C)
+#' # Returns FALSE
+#' is.diag(X = A)
+#' is.diag(X = B)
+#' @references
+#' [Wikipedia: Diagonal matrix](https://en.wikipedia.org/wiki/Diagonal_matrix)
+#' @export
+is.diag <- function(X,
+                    tol = 1e-8,
+                    stop = FALSE) {
+  test.square <- is.square(
+    X = X,
+    stop = stop
+  )
+  if (!test.square) {
+    return(FALSE)
+  } else {
+    diag(X) <- rep(
+      x = 0,
+      length = nrow(X)
+    )
+    test <- sum(as.vector(X)) <= tol
+    if (!test) {
+      if (stop) {
+        stop(
+          "\'X\' is not a diagonal matrix."
+        )
+      }
+    }
+  }
+  test
+}
+
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Symmetric Matrix
 #'
 #' @description Checks if a matrix is symmetric.
 #'
@@ -51,7 +130,6 @@ is.square <- function(X,
 #'   with any of the following conditions:
 #'   `X` is not a square matrix or
 #'   `X` is not a symmetric matrix.
-#' @author Ivan Jacob Agaloos Pesigan
 #' @return
 #'   Returns `FALSE`,
 #'   if the `X` is not square and not symmetric.
@@ -89,15 +167,17 @@ is.symmetric <- function(X,
   test
 }
 
-#' Positive Definite
+## Add lower triangular and upper triangular in the future
+
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Positive Definite
 #'
 #' @description Checks if eigenvalues in a square matrix are positive.
 #'
-#' @author Ivan Jacob Agaloos Pesigan
+#' @inheritParams is.diag
 #' @param X Numeric matrix.
 #'   Symmetric matrix.
-#' @param tol Numeric.
-#'   Tolerance.
 #' @param stop Logical.
 #'   If `TRUE`,
 #'   stops and returns an error
@@ -161,11 +241,12 @@ is.positive.definite <- function(X,
   test
 }
 
-#' Invertible Matrix
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Invertible Matrix
 #'
 #' @description Checks if a square matrix is invertible.
 #'
-#' @author Ivan Jacob Agaloos Pesigan
 #' @param X Numeric matrix.
 #'   Square matrix.
 #' @param stop Logical.
@@ -224,7 +305,9 @@ is.invertible <- function(X,
   test
 }
 
-#' Singular Matrix
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Singular Matrix
 #'
 #' @description Checks if a square matrix is non invertible or singular.
 #'
